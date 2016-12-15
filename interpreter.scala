8c4adfb23894801b0d041e61a29c2993dd3a652e
Abhinav <abhinav@kamcord.com>
2016-12-15 16:13:51 -0500

    Update interpreter.scala

--- interpreter.scala
+++ interpreter.scala
@@ -1,34 +1,8 @@
-object hw12 extends js.util.JsApp {
-  import js.hw12.ast._
-  import js.hw12._
+object interpreter extends js.util.JsApp {
+  import js.repr.ast._
+  import js.repr._
   import js.util.State
   import scala.util.parsing.input.NoPosition
-  /*
-   * CSCI-UA.0480-003: Homework 12
-   * Abhinav Havaldar
-   *
-   * Partner: Rhishab Ranawat
-   * Collaborators: <Any Collaborators>
-   */
-
-  /*
-   * Fill in the appropriate portions above by replacing things delimited
-   * by '<'... '>'.
-   *
-   * Replace the '???' expression with your code in each function.
-   *
-   * Do not make other modifications to this template, such as
-   * - adding "extends App" or "extends Application" to your Lab object,
-   * - adding a "main" method, and
-   * - leaving any failing asserts.
-   *
-   * Your solution will _not_ be graded if it does not compile!!
-   *
-   * This template compiles without error. Before you submit comment out any
-   * code that does not compile or causes a failing assert.  Simply put in a
-   * '???' as needed to get something that compiles without error.
-   *
-   */
 
   /* Type Inference */
 
